f8ad8b8c14e858bbe3954c347ef259e409fc8b49
DuckBoy102 <avieln7@gmail.com>
2022-03-10 15:32:08 +0200

    hay

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,19 +12,25 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Enter the first student's name");
             string name = Console.ReadLine();
-            Console.WriteLine("Enter the student's ID");
+            Console.WriteLine("Enter the first student's ID");
             int ID = int.Parse(Console.ReadLine());
-            Student FirstStudent = new Student(name, ID, 0);
+            Student FirstStudent = new Student(name, ID, -1);
+            FirstStudent.CalculateAverage();
+            Console.WriteLine("The first student's average grade is {0}", FirstStudent.Grade);
             Console.WriteLine("Enter the second student's name");
             string secondname = Console.ReadLine();
             Console.WriteLine("Enter the second student's ID");
             int secondID = int.Parse(Console.ReadLine());
-            Student SecondStudent = new Student(secondname, secondID, 5);
+            Student SecondStudent = new Student(secondname, secondID, -1);
+            SecondStudent.CalculateAverage();
+            Console.WriteLine("The second student's average grade is {0}", SecondStudent.Grade);
             Console.WriteLine("Enter the thired student's name");
             string thiredname = Console.ReadLine();
             Console.WriteLine("Enter the thired student's ID");
             int thiredID = int.Parse(Console.ReadLine());
-            Student ThiredStudent = new Student(thiredname, thiredID, 10);
+            Student ThiredStudent = new Student(thiredname, thiredID, -1);
+            ThiredStudent.CalculateAverage();
+            Console.WriteLine("The thired student's average grade is {0}", ThiredStudent.Grade);
         }
     }
 }
