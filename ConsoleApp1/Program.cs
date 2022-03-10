9fcadb51fd0a1bac0d823a91a9f00cb51a924266
DuckBoy102 <avieln7@gmail.com>
2022-03-10 15:17:22 +0200

    hay

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the first student's name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the student's ID");
+            int ID = int.Parse(Console.ReadLine());
+            Student FirstStudent = new Student(name, ID, 0);
+            Console.WriteLine("Enter the second student's name");
+            string secondname = Console.ReadLine();
+            Console.WriteLine("Enter the second student's ID");
+            int secondID = int.Parse(Console.ReadLine());
+            Student SecondStudent = new Student(secondname, secondID, 5);
+            Console.WriteLine("Enter the thired student's name");
+            string thiredname = Console.ReadLine();
+            Console.WriteLine("Enter the thired student's ID");
+            int thiredID = int.Parse(Console.ReadLine());
+            Student ThiredStudent = new Student(thiredname, thiredID, 10);
         }
     }
 }
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        private string fullname;
+
+        public string FullName
+        {
+            get { return fullname; }
+            set { fullname = value; }
+        }
+        private int id;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private float grade;
+
+        public float Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+        public Student(string Name, int ID, int Grade)
+        {
+            this.FullName = Name;
+            this.ID = ID;
+            this.Grade = Grade;
+        }
+        public void CalculateAverage()
+        {
+            int a, b, c, d, e;
+            Console.WriteLine("Enter 5 grades of the student");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+            grade = (a + b + c + d + e) / 5;
+        }
+    }
+}
